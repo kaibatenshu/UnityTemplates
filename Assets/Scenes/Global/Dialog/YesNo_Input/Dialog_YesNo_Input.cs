98109500270a450a6a5ff1a7146dbc5529915049
Đức Trí <tri.duong@playgroundvina.com>
2023-03-16 16:39:20 +0700

    Login and Parse with Google

--- Assets/Scenes/Global/Dialog/YesNo_Input/Dialog_YesNo_Input.cs
+++ Assets/Scenes/Global/Dialog/YesNo_Input/Dialog_YesNo_Input.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dialog_YesNo_Input : MonoBehaviour
-{
+public class Dialog_YesNo_Input : MonoBehaviour{
     // Start is called before the first frame update
     void Start()
     {
