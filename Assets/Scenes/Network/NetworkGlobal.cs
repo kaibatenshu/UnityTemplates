897365dff173c5fa47ce43be52758abc6ec66ab3
Đức Trí <tri.duong@playgroundvina.com>
2023-03-17 12:01:43 +0700

    ok

--- Assets/Scenes/Network/NetworkGlobal.cs
+++ Assets/Scenes/Network/NetworkGlobal.cs
@@ -1,20 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
-public class NetworkGlobal : MonoBehaviour
-{
+public class NetworkGlobal : MonoBehaviour{
+    private System.Object syncLock;
+    private Action actionUpdateUI;
+    public void setUpdateUI(Action _action){
+        new Thread(new ThreadStart(()=>{
+            lock(syncLock){
+                while(actionUpdateUI!=null)
+                    Thread.Sleep(1);
+                actionUpdateUI=_action;
+            }
+        })).Start();
+    }
+    IEnumerator doActionUIGame(Action _action) { _action(); yield break; }
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        syncLock = new System.Object();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if(actionUpdateUI!=null){
+            StartCoroutine(doActionUIGame(actionUpdateUI));
+            actionUpdateUI=null;
+        }
     }
+
+
+
+
+
 
 
     private static NetworkGlobal ins = null;
